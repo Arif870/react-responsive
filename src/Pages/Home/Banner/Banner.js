2e43c593cce4f09d143dc4905765135eb1efc107
Arif870 <arifuzzaman959@gmail.com>
2021-10-12 17:29:42 +0600

    react bootstrap slider added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import banner1 from "../../../images/banner/banner1.jpg";
+import banner2 from "../../../images/banner/banner2.jpg";
+import banner3 from "../../../images/banner/banner3.jpg";
+
+export default function Banner() {
+  return (
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner1} alt="First slide" />
+          <Carousel.Caption>
+            <h1 className="text-info ">Techinal Experts</h1>
+            <p className="text-white">
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner2} alt="Second slide" />
+
+          <Carousel.Caption>
+            <h1 className="text-info ">Satisfaction Guaranteed</h1>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner3} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h1 className="text-info">Foolish Free of Cost</h1>
+            <p className="text-white">
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+}
